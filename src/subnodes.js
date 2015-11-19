263d61ca83fbcd0dcbb0cf5c0b6f0ae0e15b355e
lammas <lammas@gmail.com>
2015-11-19 16:51:41 +0200

    Fix: Less convoluted subnodes accessor

--- src/subnodes.js
+++ src/subnodes.js
@@ -1,34 +1,17 @@
 module.exports = function(node) {
-	switch (node.type) {
-		case 'root':
-		case 'scope':
-			return node.statements;
+	var subnodes = [];
+	for (var param in node) {
+		if (!node.hasOwnProperty(param) || node[param] === null)
+			continue;
+		if (param == 'parent')
+			continue;
 
-		case 'preprocessor':
-			if (node.guarded_statements)
-				return node.guarded_statements;
-			return null;
-
-		case 'function_declaration':
-			return [node.body];
-
-		case 'if_statement':
-		case 'for_statement':
-		case 'while_statement':
-		case 'do_statement':
-			var nodes = [node.body];
-			if (node.elseBody)
-				nodes.push(node.elseBody);
-			return nodes;
-
-		case 'declarator':
-			return [node.typeAttribute].concat(node.declarators);
-
-		case 'declarator_item':
-			return [node.name];
-
-		// TODO: rest of the subnode accessors
-		default:
-			return null;
+		if (node[param] instanceof Array) {
+			subnodes = subnodes.concat(node[param]);
+		}
+		else if (typeof(node[param]) == 'object') {
+			subnodes.push(node[param]);
+		}
 	}
+	return subnodes;
 };
